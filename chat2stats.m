10360c6546dabb1819fd255475ac1080b3783756
sparktrain <lukas.blakkan.esser@gmail.com>
2017-03-20 12:13:56 -0400

    Update chat2stats.m

--- chat2stats.m
+++ chat2stats.m
@@ -1,19 +1,43 @@
 function chat2stats(chatFile)
 % Chat Log to Chat Statistics
 %   This function takes a .txt file of a Pokémon Showdown chat log as
-%   input, runs it through all of the statistics generating functions,
+%   input, runs it through the specified statistics generating functions,
 %   then converts the resulting cell arrays to .csv files which are stored
 %   in the stats folder.
 
-cell2csv(generateChatStats(chatFile), ['stats/' chatFile(1:length(chatFile)-4) '-chatStats.csv']);
-fprintf('Chat Statistics have been successfully generated and saved.\n');
-cell2csv(generateUserGroupChatStats(chatFile), ['stats/' chatFile(1:length(chatFile)-4) '-userGroupChatStats.csv']);
-fprintf('User Group Chat Statistics have been successfully generated and saved.\n');
-cell2csv(generateTourStats(chatFile), ['stats/' chatFile(1:length(chatFile)-4) '-tourStats.csv']);
-fprintf('Tournament Statistics have been successfully generated and saved.\n');
-cell2csv(generateModStats(chatFile), ['stats/' chatFile(1:length(chatFile)-4) '-modStats.csv']);
-fprintf('Moderation Statistics have been successfully generated and saved.\n');
-cell2csv(generateQuestionStats(chatFile), ['stats/' chatFile(1:length(chatFile)-4) '-questionStats.csv']);
-fprintf('Question Statistics have been successfully generated and saved.\n');
+% Toggle these to generate specific statistics
+chatStats = true;
+userGroupChatStats = false;
+tourStats = true;
+modStats = false;
+questionStats = false;
+
+directory = 'stats/'; % Default directory is the stats folder
+logName = chatFile(1:length(chatFile)-4);
+
+if chatStats
+    cell2csv(generateChatStats(chatFile), [directory logName '-chatStats.csv']);
+    fprintf('Chat Statistics have been successfully generated and saved.\n');
+end
+
+if userGroupChatStats
+    cell2csv(generateUserGroupChatStats(chatFile), [directory logName '-userGroupChatStats.csv']);
+    fprintf('User Group Chat Statistics have been successfully generated and saved.\n');
+end
+
+if tourStats
+    cell2csv(generateTourStats(chatFile), [directory logName '-tourStats.csv']);
+    fprintf('Tournament Statistics have been successfully generated and saved.\n');
+end
+
+if modStats
+    cell2csv(generateModStats(chatFile), [directory logName '-modStats.csv']);
+    fprintf('Moderation Statistics have been successfully generated and saved.\n');
+end
+
+if questionStats
+    cell2csv(generateQuestionStats(chatFile), [directory logName '-questionStats.csv']);
+    fprintf('Question Statistics have been successfully generated and saved.\n');
+end
 
 end
